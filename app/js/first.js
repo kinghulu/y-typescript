bcb3564fc5bf90c5fe32bce8dff243302bae5257
kinghulu <578999047@qq.com>
2016-09-08 14:44:29 +0800

    first

---
+++ app/js/first.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+var num = 9;
+console.log(num);
---
+++ app/ts/first.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+const num = 9;
+console.log(num);
